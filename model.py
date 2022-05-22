991767eb90449640443389c6450e6e3a73f6dbec
omaranwar21 <omarahmed11185@gmail.com>
2022-05-22 18:02:08 +0200

    Revert "Merge pull request #4 from omaranwar21/Machine-Learning"
    This reverts commit fbe88c9e93ed1785ed606703d3e52d1c6018a25c, reversing
    changes made to 16419a66b3676e83975a24890feacc5d472da7b2.

--- model.py
+++ model.py
@@ -15,7 +15,7 @@ dataframe.info()
 data_modified = dataframe.drop(labels=["Unnamed: 32", "id"], axis=1)
 data_modified['diagnosis'] = data_modified['diagnosis'].map({'M': 1, 'B': 0})
 
-# Remove outliers.
+# Remove outliers
 z_scores = zscore(data_modified)
 abs_z_scores = np.abs(z_scores)
 filtered_entries = (abs_z_scores < 3).all(axis=1)
@@ -26,7 +26,6 @@ x = data_modified.drop("diagnosis", axis=1)
 y = data_modified["diagnosis"]
 
 
-# A function to select the best features by checking their correlation.
 def manualFeatureSelection(features, outcome, r=0.76, pro=0.01, corrType='p'):
     # First filter
     # Check correlation between all features and the outcome.
@@ -38,13 +37,14 @@ def manualFeatureSelection(features, outcome, r=0.76, pro=0.01, corrType='p'):
             rho, p = pearsonr(features[str(i)], outcome)
 
         if (abs(rho) > r) and (p < pro):
+            # print("is " + li)
             x1.append(i)
     x2 = features[x1]
     # -----------------------------------------------------------------------------
     # Second filter
     # Check correlation between all features and each other to eliminate redundant features.
 
-    corrColumns = set()  # Set of all the names of correlated columns
+    col_corr = set()  # Set of all the names of correlated columns
 
     if corrType == 's':
         corr_matrix = x2.corr(method='spearman')
@@ -54,27 +54,23 @@ def manualFeatureSelection(features, outcome, r=0.76, pro=0.01, corrType='p'):
         for j in range(i):
             if abs(corr_matrix.iloc[i, j]) > r:  # we are interested in absolute coeff value
                 colname = corr_matrix.columns[i]  # getting the name of column
-                corrColumns.add(colname)
-    return list(corrColumns)
+                col_corr.add(colname)
+    return list(col_corr)
 
 
-# Best manual features selected after many experiments were:
-# ['perimeter_worst', 'area_worst', 'radius_worst', 'concave points_worst']
-x_featuresManual = ['perimeter_worst', 'area_worst', 'radius_worst', 'concave points_worst']
+x_featuresManual = manualFeatureSelection(x, y)
 
 # Split data into (70%) train data and (30%) test data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
 
-
-# As the best model which was used is the random forest model with applying GridSearchCV to tune its hyperparameters,
-# To avoid overfitting, And selecting manual selected features, After many experiments.
+# As the best model which was used is the random forest model with applying GridSearchCV to tune its hyperparameters.
+# And selecting manual selected features, After many experiments.
 
 
 breastCancerModel = RandomForestClassifier(max_depth=3,
                                            max_features=2,
                                            min_samples_split=25,
                                            n_estimators=500)
-
 breastCancerModel.fit(x_train[x_featuresManual], y_train)
 
 print(accuracy_score(y_test, breastCancerModel.predict(x_test[x_featuresManual])))
