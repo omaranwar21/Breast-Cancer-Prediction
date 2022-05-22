5ff35d011f4b56eb0e2c534b976074e588822fc3
omaranwar21 <omarahmed11185@gmail.com>
2022-05-22 17:59:09 +0200

    edits

--- wellBeing.py
+++ wellBeing.py
@@ -1,4 +1,3 @@
-# from crypt import methods
 import numpy as np
 import pickle
 from flask import Flask, url_for, render_template, request
@@ -21,15 +20,10 @@ def predict():
     features = [np.array (floatFeatures)]
     prediction = model.predict (features)
     if prediction == 1:
-        return render_template('WellBeing.html', state= "Unfortunatly", predictionText=", It is a Tumor.")
+        return render_template('WellBeing.html', predictionText= "Unfortunatly, It is a Tumor.")
     else:
-        return render_template('WellBeing.html', state= "Congratulations!", predictionText= "It is a Benign Tumor.")
+        return render_template('WellBeing.html', predictionText= "Congratulations! It is a Benign Tumor.")
        
-# @wellBeing.route('/', methods['post'])
-# def result():
-
-#     return render_template('WellBeing.html')
-
 if __name__ == "__main__":
     wellBeing.run(debug=True) 
 
